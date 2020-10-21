929813d26a9f5a0427dfa4c4fafab8db053e86c8
Mikhail Kisly <salodci@gmail.com>
2020-10-21 13:01:39 +0300

    Initial Commit

---
+++ Cinema Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinema Journal
+//
+//  Created by Homer on 10/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
