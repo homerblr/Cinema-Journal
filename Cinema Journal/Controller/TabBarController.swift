88d97737fc490147c068a2e7d123bda28a8eeeee
Mikhail Kisly <salodci@gmail.com>
2020-10-25 15:09:36 +0300

    feat: create base UI

---
+++ Cinema Journal/Controller/TabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  TabBarController.swift
+//  Cinema Journal
+//
+//  Created by Homer on 10/25/20.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.backgroundColor = .tabBarColor
+        tabBar.tintColor = .tabBarTintColor
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Cinema Journal/Helpers/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  Cinema Journal
+//
+//  Created by Homer on 10/25/20.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var tabBarColor: UIColor {
+    return UIColor(named: "tabBarBackground")!
+  }
+    
+    @nonobjc class var tabBarTintColor: UIColor {
+      return UIColor(named: "tabBarTint")!
+    }
+
+}
+
+
