9c95051c1518b901dcb287d32649783328d41443
Mikhail Kisly <salodci@gmail.com>
2020-10-29 18:12:23 +0300

    feat: implement MVC pattern to cell items, use NumberFormatter

--- Cinema Journal/Controller/ViewController.swift
+++ Cinema Journal/Controller/ViewController.swift
@@ -22,11 +22,10 @@ class ViewController: UIViewController {
         Networking.fetchData(MovieAPITarget.movies, MainData.self) { (result) in
             switch result {
             case .success(let movieData):
-                self.results = movieData.results
+                self.results.append(contentsOf: movieData.results)
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
-                print(self.results)
             case .failure(let err) :
                 print(err)
             }
@@ -38,24 +37,12 @@ class ViewController: UIViewController {
 //-MARK: TableView extensions
 extension ViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(results.count)
         return results.count
-        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: MovieCellTableViewCell.cellIdentifier, for: indexPath) as! MovieCellTableViewCell
-        cell.movieTitle.text = results[indexPath.row].title
-        cell.movieDescription.text = results[indexPath.row].overview
-        
-        if results[indexPath.row].voteAverage <= 0.0 {
-            cell.movieRate.text = "No ratings yet"
-        } else {
-            cell.movieRate.text = String(format: "%.1f", results[indexPath.row].voteAverage)
-        }
-        
-        let year = results[indexPath.row].releaseDate.dropLast(6)
-        cell.yearAndGenre.text = String(year) // need to add genre in a single string
+        cell.configureCell(results[indexPath.row])
         return cell
     }
     
--- Cinema Journal/View/MovieCellTableViewCell.swift
+++ Cinema Journal/View/MovieCellTableViewCell.swift
@@ -23,6 +23,28 @@ class MovieCellTableViewCell: UITableViewCell {
         super.awakeFromNib()
         posterImage.layer.cornerRadius = 6
         posterImage.layer.masksToBounds = true
+        
+        
+    }
+    
+    func configureCell (_ model: MovieDetails) {
+        movieTitle.text = model.title
+        movieDescription.text = model.overview
+        if model.voteAverage <= 0.0 {
+            movieRate.text = "No ratings yet"
+        } else {
+            movieRate.text = numberFormat(model.voteAverage)
+        }
+        let year = model.releaseDate.dropLast(6)
+        yearAndGenre.text = String(year) // need to add genre in a single string
+    }
+    
+    func numberFormat (_ num : Double) -> String? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 1
+        guard let stringNum = formatter.string(for: num) else {return nil}
+        return stringNum
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
