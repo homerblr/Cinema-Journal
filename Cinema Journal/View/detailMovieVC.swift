195e690821e840869cce2a4f5290b31fa12bf814
Mikhail Kisly <salodci@gmail.com>
2020-11-11 23:12:23 +0300

    init: create detailMovieVC, implement design

---
+++ Cinema Journal/View/detailMovieVC.swift
@@ -0,0 +1,42 @@
+//
+//  detailMovieVC.swift
+//  Cinema Journal
+//
+//  Created by Homer on 11/10/20.
+//
+
+import UIKit
+
+class detailMovieVC: UIViewController {
+
+    @IBOutlet var posterImage: UIImageView!
+    @IBOutlet var movieTitle: UILabel!
+    @IBOutlet var releaseYear: UILabel!
+    @IBOutlet var movieGenre: UILabel!
+    @IBOutlet var countryAndLength: UILabel!
+    @IBOutlet var starImage: UIImageView!
+    @IBOutlet var movieRating: UILabel!
+    @IBOutlet var adultNote: UILabel!
+    @IBOutlet var movieDescription: UILabel!
+    @IBOutlet var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
