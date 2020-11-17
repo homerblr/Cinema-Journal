38c0e8007ec7569afd2686c8169d80eb46cdf149
Mikhail Kisly <salodci@gmail.com>
2020-11-17 19:51:40 +0300

    init: create settingsVC, implement UserDefaults

---
+++ Cinema Journal/Model/SettingsModel.swift
@@ -0,0 +1,19 @@
+//
+//  settingsModel.swift
+//  Cinema Journal
+//
+//  Created by Homer on 11/12/20.
+//
+
+import Foundation
+
+struct SettingsModel {
+    var adult : Bool = false
+    let sections = [
+    ["Adult"],
+    ["About me", "Hire me"]
+    ]
+    
+    
+    
+}
--- Cinema Journal/View/detailMovieVC.swift
+++ Cinema Journal/View/detailMovieVC.swift
@@ -27,15 +27,4 @@ class detailMovieVC: UIViewController {
         navigationItem.title = movieTitle.text
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
